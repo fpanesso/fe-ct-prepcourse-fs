b8644f37d07fda1b53743572783a128b6d79e89e
Ferney Blandon Panesso <143520320+fpanesso@users.noreply.github.com>
2023-09-06 18:20:52 -0500

    Update Ejercicio 04.js

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -5,32 +5,50 @@
 
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
-   // Tu código:
+   // Tu código: 
+    let cuadrado = Math.pow(num,2);
+    console.log(cuadrado);
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   let cubo = Math.pow(num, 3);
+   console.log(cubo);
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   let exponent = Math.pow(num, exponent);
+   console.log(elevar);
+ 
+ 
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+     let redondeo = Math.round(redondearNumero, num);
+   console.log(redondeo);
+   
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
-   // Tu código:
+   // Tu código: 
+   let redondup = Math.ceil(redondearHaciaArriba, num);
+    console.log(redondup);
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+ 
+     let numalazar = Math.random(numeroRandom);
+    console.log(numalazar);
+
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
